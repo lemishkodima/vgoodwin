25fb4ffb34c2b634dddfd5ef444101f3b3ba4183
Lemishko Dmitry <81725793+lemishkodima@users.noreply.github.com>
2024-01-27 11:02:49 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,13 @@ import asyncio
 from aiogram.types import ChatJoinRequest, InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram import Bot, Dispatcher, F
 import logging
+import contextlib
+import asyncio
+from aiogram.types import ChatJoinRequest, InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram import Bot, Dispatcher, F
+import logging
+from googleapiclient.discovery import build
+from google.oauth2.service_account import Credentials
 
 
 BOT_TOKEN = '6524445610:AAFyCvTHI9qpKajyXzNVTNP3GCPM9jWVvZ0' 
@@ -13,18 +20,39 @@ async def approve_request (chat_join: ChatJoinRequest, bot: Bot):
    button = InlineKeyboardButton(text='ВСТУПИТЬ', url='https://t.me/+I-1JiJsJRsw1Mjk6', disable_web_page_preview=True)   
    markup = InlineKeyboardMarkup(inline_keyboard=[[button]])
 
-
+   user_data = [chat_join.from_user.id, chat_join.from_user.username, chat_join.from_user.first_name]
+   append_data_to_sheet(user_data, "1J0PTN39cYH7IRH0P_IrrJ2QcGMl_nKDVTgT6CQ34WFY", "A:C")
    await bot.send_message(chat_id=chat_join.from_user.id, text=msg, reply_markup=markup, disable_web_page_preview=True)
- 
+
+
+def append_data_to_sheet(user_data, spreadsheet_id, range_name):
+    """Добавляет данные пользователя в Google таблицу."""
+    creds = Credentials.from_service_account_file("maxim.json")
+    service = build('sheets', 'v4', credentials=creds)
+    sheet = service.spreadsheets()
+
+    request = sheet.values().append(spreadsheetId=spreadsheet_id, 
+                                    range=range_name, 
+                                    valueInputOption="USER_ENTERED", 
+                                    body={"values": [user_data]})
+    response = request.execute()
+    return response
 
 async def start():
     logging.basicConfig(level=logging.DEBUG,
-                           format="%(asctime)s - [%(levelname)s] - %(name)s -"
-                           "(%(filename)s.%(funcName)s(%(lineo)d) - %(message)s"
-                        )
+                    format="%(asctime)s - [%(levelname)s] - %(name)s -"
+                           "(%(filename)s.%(funcName)s(%(lineno)d) - %(message)s"
+                    )
     bot: Bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher ()
     dp.chat_join_request.register (approve_request, F.chat.id ==CHANNEL_ID)
+
+    try:
+     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
+    except Exception as ex:
+     logging.error( exc_info=True)
+    finally:
+     await bot.session.close()
 
     try:
      await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
