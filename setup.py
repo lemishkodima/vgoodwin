d4f5589363d775c3c7142742157c81e44ef48bb4
Lemishko Dmitry <81725793+lemishkodima@users.noreply.github.com>
2024-02-20 18:50:07 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -29,8 +29,8 @@ async def approve_request(chat_join: ChatJoinRequest, bot: Bot):
     
 @dp.message(F.text.lower() == "start")
 async def send_channel_link(message: types.Message):
-        msg = "Ваша заявка одобрена!\n\nВступить в канал: https://t.me/+2RpMM5VqAMVmNzky"
-        button = InlineKeyboardButton(text='ВСТУПИТЬ', url='https://t.me/+2RpMM5VqAMVmNzky')
+        msg = "Ваша заявка одобрена!\n\nВступить в канал: https://t.me/+Moe57nD94uU5YmEy"
+        button = InlineKeyboardButton(text='ВСТУПИТЬ', url='https://t.me/+Moe57nD94uU5YmEy')
         markup = InlineKeyboardMarkup(inline_keyboard=[[button]])
 
         await message.answer(text=msg, reply_markup=markup, disable_web_page_preview=True)
